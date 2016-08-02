a9fe81b6fedbcb48e8b467f80b52d0a6cd442ade
Shawn Zhu <shawn.zhu@level-studios.com>
2016-08-02 13:35:37 -0700

    [HW] send message action

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -10,4 +10,13 @@ export default Ember.Controller.extend({
   }),
   isValid: Ember.computed.and('validEmail', 'validMessage'),
   isDisabled: Ember.computed.not('isValid'),
+
+  actions: {
+    sendMessage() {
+      alert(`We have received your message: "${this.get('message').trim()}"; we will reach out to you via "${this.get('emailAddress')}"`);
+      this.set('responseMessage', `We got your message and we'll get in touch soon`);
+      this.set('emailAddress', '');
+      this.set('message', '');
+    }
+  }
 });
