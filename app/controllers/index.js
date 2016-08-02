f589b47ee5d950516268f4543d2efe35e6565b00
Shawn Zhu <shawn.zhu@level-studios.com>
2016-08-02 14:54:08 -0700

    set success in promise returned by save()

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -16,10 +16,14 @@ export default Ember.Controller.extend({
     saveInvitation() {
       const email = this.get('emailAddress');
       const newInvitation = this.store.createRecord('invitation', { email });
-      newInvitation.save();
-      
-      this.set('responseMessage', `Thank you! We've just saved your emailAddress: ${this.get('emailAddress')}`);
-      this.set('emailAddress', '');
+      newInvitation.save().then(response => {
+        this.set(
+          'responseMessage',
+          `Thank you! We've just saved your emailAddress: \
+          ${this.get('emailAddress')} with the following id: ${response.get('id')}`
+        );
+        this.set('emailAddress', '');
+      });
     },
   },
 });
